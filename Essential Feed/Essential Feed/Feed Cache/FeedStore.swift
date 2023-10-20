81fe6f4b4724812d68d8f2ff4657497ae3efa1c3
Zoltán Damó <zoltan.damo@gmail.com>
2023-10-20 09:30:59 +0300

    Extracted FeedStore and LocalFeedLoader definitions. New tests for proper memory management and callback handling

---
+++ Essential Feed/Essential Feed/Feed Cache/FeedStore.swift
@@ -0,0 +1,16 @@
+//
+//  FeedStore.swift
+//  Essential Feed
+//
+//  Created by Zoltan Damo on 20/10/2023.
+//
+
+import Foundation
+
+ public protocol FeedStore {
+     typealias DeletionCompletion = (Error?) -> Void
+     typealias InsertionCompletion = (Error?) -> Void
+
+     func deleteCachedFeed(completion: @escaping DeletionCompletion)
+     func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+ }
