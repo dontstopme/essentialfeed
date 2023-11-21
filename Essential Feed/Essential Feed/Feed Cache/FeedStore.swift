d5be7c25b143c78fd05fa94267229440315a364c
Zoltán Damó <zoltan.damo@gmail.com>
2023-11-21 17:59:46 +0200

    Extended FeedStore to retrieve cached images, using calendars to manipulate  date objects, moved FeedstoreSpy into a separate file

--- Essential Feed/Essential Feed/Feed Cache/FeedStore.swift
+++ Essential Feed/Essential Feed/Feed Cache/FeedStore.swift
@@ -7,10 +7,18 @@
 
 import Foundation
 
- public protocol FeedStore {
-     typealias DeletionCompletion = (Error?) -> Void
-     typealias InsertionCompletion = (Error?) -> Void
+public enum RetrieveCachedFeedResult {
+    case empty
+    case found(feed: [LocalFeedImage], timestamp: Date)
+    case failure(Error)
+}
 
-     func deleteCachedFeed(completion: @escaping DeletionCompletion)
-     func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion)
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+    typealias RetrievalCompletion = (RetrieveCachedFeedResult) -> Void
+
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion)
+    func retrieve(completion: @escaping RetrievalCompletion)
  }
