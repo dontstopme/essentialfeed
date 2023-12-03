0956f8200c401dc5221ac852259131d0b1e98194
Zoltán Damó <zoltan.damo@gmail.com>
2023-12-03 18:02:53 +0200

    Created test target for integration tests

---
+++ Essential Feed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,12 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Zoltan Damo on 03/12/2023.
+//
+
+import XCTest
+
+class EssentialFeedCacheIntegrationTests: XCTestCase {
+
+}
