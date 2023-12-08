8db4e493dc63df445298166edddc70549c94f750
Zoltán Damó <zoltan.damo@gmail.com>
2023-12-08 08:30:43 +0200

    Rendering logic for Image Feed UI using MVC pattern

---
+++ Essential Feed/EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Zoltan Damo on 08/12/2023.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
