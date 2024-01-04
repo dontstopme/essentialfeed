1f505f9e9db28de9d0332a1c59f3204a63a5df32
Zoltan Damo <zoltan.damo@gmail.com>
2024-01-04 11:11:31 +0200

    move FeedImageDataCache into production

---
+++ Essential Feed/Essential Feed/Feed Feature/FeedImageDataCache.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageDataCache.swift
+//  EssentialFeed
+//
+//  Created by Zoltan Damo on 04.01.2024.
+//
+
+import Foundation
+
+public protocol FeedImageDataCache {
+    typealias Result = Swift.Result<Void, Error>
+    
+    func save(_ data: Data, for url: URL, completion: @escaping (Result) -> Void)
+}
