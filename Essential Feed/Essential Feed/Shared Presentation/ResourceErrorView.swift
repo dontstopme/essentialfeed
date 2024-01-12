01cf21a8569f9f8da5930f53403f4fe3cf84eaf9
Zoltan Damo <zoltan.damo@gmail.com>
2024-01-12 06:24:12 +0200

    renamed and moved error view into the shared presentation space

---
+++ Essential Feed/Essential Feed/Shared Presentation/ResourceErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  ResourceErrorView.swift
+//  EssentialFeed
+//
+//  Created by Zoltan Damo on 12.01.2024.
+//
+
+import Foundation
+
+public protocol ResourceErrorView {
+    func display(_ viewModel: ResourceErrorViewModel)
+}
