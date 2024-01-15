02f77bffa8f51106bc060dd5387074605ea5e127
Zoltan Damo <zoltan.damo@gmail.com>
2024-01-15 10:15:06 +0200

    Implement Image Comments UI

---
+++ Essential Feed/EssentialFeediOS/Image Comments UI/Views/ImageCommentCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImageCommentCell.swift
+//  EssentialFeediOS
+//
+//  Created by Zoltan Damo on 15.01.2024.
+//
+
+import UIKit
+
+public final class ImageCommentCell: UITableViewCell {
+    @IBOutlet private(set) public var messageLabel: UILabel!
+    @IBOutlet private(set) public var usernameLabel: UILabel!
+    @IBOutlet private(set) public var dateLabel: UILabel!
+}
