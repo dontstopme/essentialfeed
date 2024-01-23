cfdd50a695f820e55cf32657d26d2d3de6e925b5
Zoltan Damo <zoltan.damo@gmail.com>
2024-01-23 14:12:10 +0200

    Make LocalFeedLoader.load sync

--- Essential Feed/Essential Feed/Feed Cache/LocalFeedLoader.swift
+++ Essential Feed/Essential Feed/Feed Cache/LocalFeedLoader.swift
@@ -26,17 +26,14 @@ extension LocalFeedLoader: FeedCache {
 }
 
 extension LocalFeedLoader {
-    public typealias LoadResult = Swift.Result<[FeedImage], Error>
+    public func load() throws -> [FeedImage] {
 
-    public func load(completion: @escaping (LoadResult) -> Void) {
-        completion(LoadResult {
-            if let cache = try store.retrieve(), FeedCachePolicy.validate(cache.timestamp, against: currentDate()) {
-                return cache.feed.toModels()
-            }
+        if let cache = try store.retrieve(), FeedCachePolicy.validate(cache.timestamp, against: currentDate()) {
+            return cache.feed.toModels()
+        }
 
-            return []
-        })
-	}
+        return []
+    }
 }
 
 extension LocalFeedLoader {
--- Essential Feed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ Essential Feed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -147,19 +147,11 @@ class EssentialFeedCacheIntegrationTests: XCTestCase {
     }
 
     private func expect(_ sut: LocalFeedLoader, toLoad expectedFeed: [FeedImage], file: StaticString = #filePath, line: UInt = #line) {
-        let exp = expectation(description: "Wait for load completion")
-        sut.load { result in
-            switch result {
-            case let .success(loadedFeed):
-                XCTAssertEqual(loadedFeed, expectedFeed, file: file, line: line)
-
-            case let .failure(error):
-                XCTFail("Expected successful feed result, got \(error) instead", file: file, line: line)
-            }
-
-            exp.fulfill()
+        do {
+            _ = try sut.load()
+        } catch {
+            XCTFail("Expected successful feed result, got \(error) instead", file: file, line: line)
         }
-        wait(for: [exp], timeout: 1.0)
     }
 
     private func save(_ data: Data, for url: URL, with loader: LocalFeedImageDataLoader, file: StaticString = #filePath, line: UInt = #line) {
